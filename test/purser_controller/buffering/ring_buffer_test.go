cc9d9a36ba207460bfddc6c9f53c4f039f757970
KrishnaKarthik <kreddyj@vmware.com>
2018-08-21 10:25:04 +0530

    add tests for Put, Get functions in ring_buffer

---
+++ test/purser_controller/buffering/ring_buffer_test.go
@@ -0,0 +1,34 @@
+package buffering_test
+
+import (
+	"testing"
+	"github.com/vmware/purser/pkg/purser_controller/buffering"
+	"github.com/vmware/purser/test/utils"
+	"sync"
+)
+
+func TestPut(t *testing.T) {
+	// use Put to add one more, return from Put should be True
+	r := &buffering.RingBuffer{Size: 2, Mutex: &sync.Mutex{}}
+
+	testValue1 := 1
+	ret1 := r.Put(testValue1)
+	utils.Assert(t, ret1, "inserting into not full buffer")
+
+	testValue2 := 38
+	ret2 := r.Put(testValue2)
+	utils.Assert(t, !ret2, "inserting into full buffer")
+}
+
+func TestGet(t *testing.T) {
+	// use Put to add one more, return from Put should be True
+	r := &buffering.RingBuffer{Size: 2, Mutex: &sync.Mutex{}}
+
+	ret1 := r.Get()
+	utils.Assert(t, ret1 == nil, "get elements of empty buffer")
+
+	testValue := 1
+	r.Put(testValue)
+	ret2 := r.Get()
+	utils.Assert(t, (*ret2).(int) == testValue, "get elements from non empty buffer")
+}
