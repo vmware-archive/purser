f63ae657dbf3157e2da18717d462d82bd5cd8a15
Gurusreekanth C <gurusreekanthc@vmware.com>
2018-10-10 01:01:01 +0530

    Implemented the following.  - Persisting containers and its relation to Pod.  - Helper functions for building interaction graphs for pod-to-pod    and service-to-service communication.

--- pkg/controller/persistence/dgraph/dgraph.go
+++ pkg/controller/persistence/dgraph/dgraph.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018 VMware Inc. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dgraph
 
 import (
@@ -43,8 +60,9 @@ func CreateSchema() error {
 	op.Schema = `
 		name: string @index(term) .
 		xid:  string @index(term) .
-		isService: string @index(term) .
-		isPod: string @index(term) .
+		isService: bool .
+		isPod: bool .
+		isContainer: bool .
 	`
 	ctx := context.Background()
 	err := Client.Alter(ctx, op)
@@ -87,14 +105,12 @@ func GetUId(dg *dgo.Dgraph, id string, nodeType string) (string, error) {
 	return r.IDs[0].UID, nil
 }
 
-func MutateNode(dg *dgo.Dgraph, n []byte) error {
+func MutateNode(dg *dgo.Dgraph, n []byte) (*api.Assigned, error) {
 	mu := &api.Mutation{
 		CommitNow: true,
 	}
 
 	mu.SetJson = n
 	ctx := context.Background()
-	_, err := dg.NewTxn().Mutate(ctx, mu)
-
-	return err
+	return dg.NewTxn().Mutate(ctx, mu)
 }
