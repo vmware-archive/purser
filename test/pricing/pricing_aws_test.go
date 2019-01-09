f49708f19110e3498ffe739d08aad210453f3a2b
Krishna Karthik Reddy J <kreddyj@vmware.com>
2019-01-09 17:13:52 +0530

    refactor and fix make check error

--- test/pricing/pricing_aws_test.go
+++ test/pricing/pricing_aws_test.go
@@ -3,6 +3,8 @@ package pricing
 import (
 	"testing"
 
+	"github.com/vmware/purser/test/utils"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/vmware/purser/pkg/controller/dgraph"
 	"github.com/vmware/purser/pkg/controller/dgraph/models"
@@ -10,11 +12,29 @@ import (
 )
 
 // TestAWSPricingFlow it should populate your dgraph running at localhost 9080 port with aws compute and storage prices
+// The following dgraph query will give the rate card data
+// {
+//		rateCard(func: has(isRateCard)) {
+//			cloudProvider
+//			region
+//			nodePrices {
+//				instanceType
+//				operatingSystem
+//				price
+//				instanceFamily
+//			}
+//			storagePrices {
+//				volumeType
+//				usageType
+//				price
+//			}
+//		}
+// }
 func TestAWSPricingFlow(t *testing.T) {
 	logrus.SetLevel(logrus.DebugLevel)
 	dgraph.Start("localhost", "9080")
-	var rateCard *models.RateCard
-	rateCard = aws.GetRateCardForAWS("us-east-1")
+	rateCard := aws.GetRateCardForAWS("us-east-1")
 	models.StoreRateCard(rateCard)
 	defer dgraph.Close()
+	utils.Assert(t, rateCard != nil, "rate card is nil")
 }
