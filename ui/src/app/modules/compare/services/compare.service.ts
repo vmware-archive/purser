5b76ab8436f637c5c7e009fa3ad55229afda9188
anna-delta <anerid@vmware.com>
2019-07-31 18:00:16 +0530

    Added function to post cloud-selectedRegion data and get cloud details

--- ui/src/app/modules/compare/services/compare.service.ts
+++ ui/src/app/modules/compare/services/compare.service.ts
@@ -14,8 +14,7 @@ export class CompareService{
 
     regions : CloudRegion[] = [];
     getRegionsUrl = BACKEND_URL + "clouds/regions";
-    getCloudUrl = BACKEND_URL + "clouds/compare";
-    postCloudRegion = BACKEND_URL + "clouds/send/region";
+    postCloudRegion = BACKEND_URL + "clouds/compare";
 
     constructor(private http: HttpClient){ }
 
@@ -23,12 +22,9 @@ export class CompareService{
         return this.http.get<any>(this.getRegionsUrl);     
     }
 
-    getCloudDetails() : Observable<any>{
-        return this.http.get<any>(this.getCloudUrl);
-    }
-
-    sendCloudRegion():Observable<any>{
-      return this.http.post<any>(this.postCloudRegion,null);
+    //returns all the clouds details
+    sendCloudRegion(sendCloudRegion):Observable<any>{
+      return this.http.post<any>(this.postCloudRegion,sendCloudRegion);
     }
     
 }
