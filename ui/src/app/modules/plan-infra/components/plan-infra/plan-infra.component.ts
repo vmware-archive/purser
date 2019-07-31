cef10eff2bef9a9b0f801428bf414d82b0082c40
anna-delta <anerid@vmware.com>
2019-08-01 03:57:02 +0530

    Module for planning infra was added

--- ui/src/app/modules/plan-infra/components/plan-infra/plan-infra.component.ts
+++ ui/src/app/modules/plan-infra/components/plan-infra/plan-infra.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanInfraComponent implements OnInit {
 
+  cloudRegions = [
+    {
+      cloud : "Amazon Web Services",
+      region : ["US-East-1", "US-West-2", "EU-West-1"],
+      selectedRegion : "US-East-1"
+    },
+    {
+      cloud : "Google Cloud Platform",
+      region : ["US-East-1", "US-West-2", "EU-West-1"],
+      selectedRegion : "US-East-1"      
+    },
+    {
+      cloud : "Pivotal Container Service",
+      region : ["US-East-1", "US-West-2", "EU-West-1"],
+      selectedRegion : "US-East-1"
+    },
+    {
+      cloud : "Microsoft Azure",
+      region : ["US-East-1", "US-West-2", "EU-West-1"] ,
+      selectedRegion : "US-East-1"     
+    }
+  ];
+
+  images = ["awst.png", "gcpt.png", "pkst.png", "azuret.png"];
+
+  myStyles = [{
+    'background-color': '#FEF3B5',
+    },
+    {
+      'background-color': '#E1F1F6',
+    },
+    {
+      'background-color': '#DFF0D0',
+    },
+    {
+      'background-color': '#F5DBD9',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
--- ui/src/app/modules/plan-infra/plan-infra.module.ts
+++ ui/src/app/modules/plan-infra/plan-infra.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlanInfraComponent } from './components/plan-infra/plan-infra.component';
+import { ClarityModule, ClrFormsNextModule } from '@clr/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ClarityModule,
+    ClrFormsNextModule,
+    FormsModule,
+    RouterModule
   ],
   declarations: [PlanInfraComponent]
 })
