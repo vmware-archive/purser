c02331799dd9d1c8c53b8b6bd37917b605c74d10
anna-delta <anerid@vmware.com>
2019-08-01 01:51:43 +0530

    plan-infra module was added

---
+++ ui/src/app/modules/plan-infra/components/plan-infra/plan-infra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-infra',
+  templateUrl: './plan-infra.component.html',
+  styleUrls: ['./plan-infra.component.scss']
+})
+export class PlanInfraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ui/src/app/modules/plan-infra/plan-infra.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlanInfraComponent } from './components/plan-infra/plan-infra.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PlanInfraComponent]
+})
+export class PlanInfraModule { }
