b4c9f42377c314925b59938a509a1398e3fc3330
anna-delta <anerid@vmware.com>
2019-07-31 17:02:46 +0530

    Added functions for GET and POST methods

--- ui/src/app/modules/compare/services/compare.service.ts
+++ ui/src/app/modules/compare/services/compare.service.ts
@@ -13,11 +13,22 @@ import { CloudRegion } from '../components/compare-clouds/cloud-region';
 export class CompareService{
 
     regions : CloudRegion[] = [];
-    getRegionsUrl = BACKEND_URL + "clouds/regions/"
+    getRegionsUrl = BACKEND_URL + "clouds/regions";
+    getCloudUrl = BACKEND_URL + "clouds/compare";
+    postCloudRegion = BACKEND_URL + "clouds/send/region";
 
     constructor(private http: HttpClient){ }
 
     getRegions() : Observable<any>{
         return this.http.get<any>(this.getRegionsUrl);     
     }
+
+    getCloudDetails() : Observable<any>{
+        return this.http.get<any>(this.getCloudUrl);
+    }
+
+    sendCloudRegion():Observable<any>{
+      return this.http.post<any>(this.postCloudRegion,null);
+    }
+    
 }
